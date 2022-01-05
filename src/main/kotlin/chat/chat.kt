17f2413226e5b774811a1d77da85ace3980bcece
NMagpie <sorochin.work@gmail.com>
2022-01-06 00:32:06 +0200

    Basic features of the project

---
+++ src/main/kotlin/chat/chat.kt
@@ -0,0 +1,53 @@
+package chat
+
+import io.ktor.client.features.websocket.*
+import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import main.client
+import main.user
+import security.login
+import security.tryToLogin
+
+suspend fun initChat() {
+    if (user.username != "") {
+        runBlocking {
+            client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+                val messageOutputRoutine = launch { outputMessages() }
+                val userInputRoutine = launch { inputMessages() }
+
+                userInputRoutine.join()
+                messageOutputRoutine.cancelAndJoin()
+            }
+        }
+        client.close()
+        println("Connection closed. Goodbye!")
+    } else tryToLogin()
+}
+
+suspend fun DefaultClientWebSocketSession.outputMessages() {
+    try {
+        for (message in incoming) {
+            message as? Frame.Text ?: continue
+            val receivedText = message.readText()
+            println(receivedText)
+        }
+    } catch (e: Exception) {
+        println("Error while receiving: " + e.localizedMessage)
+    }
+}
+
+suspend fun DefaultClientWebSocketSession.inputMessages() {
+    while (true) {
+        val message = readLine() ?: ""
+        if (message.equals("exit", true)) return
+        try {
+            send(message)
+        } catch (e: Exception) {
+            println("Error while sending: " + e.localizedMessage)
+            return
+        }
+    }
+}
